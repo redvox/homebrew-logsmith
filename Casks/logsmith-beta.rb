3e0c4e281ace239d4734c968fe522bacb9ea8e65
redvox <jens.gohl@otto.de>
2026-02-20 16:11:21 +0100

    Revert "update cask"
    This reverts commit d245d959155962f3ea1415d2b1afc82a5a2609e6.

--- Casks/logsmith-beta.rb
+++ Casks/logsmith-beta.rb
@@ -1,7 +1,7 @@
 cask "logsmith-beta" do
-  version "11.0.0-rc.1"
+  version "10.1.0-rc.3"
   homepage "https://github.com/otto-de/logsmith"
-  sha256 "7e58d15f3a6f0c9b99d0221354ad667463b09255e175555565aef2b1b1d86b67"
+  sha256 "62584c7763216c365173d2784226f97d7393425a87e3fa86f6363c27621dd24b"
   url "https://github.com/otto-de/logsmith/releases/download/#{version}/logsmith_darwin_#{version}.zip"
   app "dist/logsmith.app"
 end
