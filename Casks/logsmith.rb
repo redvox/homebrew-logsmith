a33b4962c9d337cf0f28b2bc7561dca8ec85fe8a
Jens Gohl <jens@copperlion.de>
2026-03-30 07:57:59 +0000

    update cask

--- Casks/logsmith.rb
+++ Casks/logsmith.rb
@@ -1,7 +1,7 @@
 cask "logsmith" do
-  version "11.0.0"
+  version "11.0.1"
   homepage "https://github.com/otto-de/logsmith"
-  sha256 "7fe2ed1379b36bb8d4c4cb0afb1d4b0a94bde8f2734147124731ac6b50864aba"
+  sha256 "3e7f97bfeda382cc932a855d9d7f2ce1e444ab3ebbff25c2988d20ad6521c0d9"
   url "https://github.com/otto-de/logsmith/releases/download/#{version}/logsmith_darwin_#{version}.zip"
   app "dist/logsmith.app"
 end
